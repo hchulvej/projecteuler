65e83cc0d8aa150e4263c5c56f6bffa6e0cbe50e
hchulvej <hchulvej@gmail.com>
2023-08-29 21:14:04 +0200

    Finished solution

--- euler_007.java
+++ euler_007.java
@@ -13,6 +13,7 @@ public class euler_007 {
     }
 
     public void updatePrimes() {
+        noOfPrimes = 0;
         for (int i = 0; i < this.primes.length; i++) {
             if (primes[i]) {
                 noOfPrimes++;
@@ -25,6 +26,16 @@ public class euler_007 {
         }
     }
 
+    public boolean[] expendPrimes() {
+        int N = this.primes.length;
+        boolean[] newPrimes = new boolean[10*N];
+        Arrays.fill(newPrimes, true);
+        for (int i = 0; i < N; i++) {
+            newPrimes[i] = this.primes[i];
+        }
+        return newPrimes;
+    }
+
     public int getNoOfPrimes() {
         return this.noOfPrimes;
     }
@@ -33,14 +44,30 @@ public class euler_007 {
 
         long startTime = System.nanoTime();
 
-        euler_007 euler = new euler_007(100);
+        euler_007 euler = new euler_007(1000);
+
+        while (euler.getNoOfPrimes() < 10001) {
+            euler.updatePrimes();
+            euler.primes = euler.expendPrimes();
+        }
         euler.updatePrimes();
 
-        int np = euler.getNoOfPrimes();
+        int count = 0;
+        int p = 0;
+
+        while(true) {
+            if (euler.primes[p]) {
+                count++;
+            }
+            if (count == 10001) {
+                break;
+            }
+            p++;
+        }
 
         long duration = System.nanoTime() - startTime;
 
-        System.out.println("The 10001th prime is " + np + ". Runtime took around " + duration
+        System.out.println("The 10001th prime is " + p + ". Runtime took around " + duration
                 + " nanoseconds.");
     }
 
