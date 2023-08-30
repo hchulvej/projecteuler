a75706d09749e82cd276a6af0efd8101a9f34c40
hchulvej <hchulvej@gmail.com>
2023-08-30 11:35:39 +0200

    This is wrong

--- euler_008.java
+++ euler_008.java
@@ -4,17 +4,32 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class euler_008 {
-    
+
     private static BufferedInputStream bis = null;
-    private static String path = "C:\\Java\\projecteuler\\euler_008.txt";
+    private static String path = "C:\\test\\projecteuler\\euler_008.txt";
+    private static byte[] digits = new byte[1000];
 
     public static void main(String[] args) {
-        
+
         try {
             bis = new BufferedInputStream(new FileInputStream(path));
-            System.out.println(Arrays.toString(bis.readNBytes(32)));
+            bis.read(digits);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        for (byte b : digits) {
+            sb.append((char) b);
+        }
+        sb.trimToSize();
+
+        String inputAsString = sb.toString();
+
+        for (int j = 0; j + 13 < inputAsString.length(); j++) {
+            String subString = inputAsString.substring(j, j + 13);
+            System.out.println(Double.parseDouble(subString));
         }
     }
 }
