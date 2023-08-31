3696e432bc59fd38ec24f356607005381f42ca76
hchulvej <hchulvej@gmail.com>
2023-08-31 14:07:47 +0200

    Not done

---
+++ euler_011.java
@@ -0,0 +1,56 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class euler_011 {
+
+    private static BufferedInputStream bis = null;
+    private static BufferedReader br = null;
+    private static String path = "C:\\test\\projecteuler\\euler_011.txt";
+    private static int[][] grid = new int[20][20];
+    private static StringBuffer input = new StringBuffer();
+
+    private static void parseInput(String str) {
+        String[] arr = str.trim().split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            int v = Integer.parseInt(arr[i]);
+            grid[i / 20][i % 20] = v;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
+        try {
+            bis = new BufferedInputStream(new FileInputStream(new File(path)));
+            br = new BufferedReader(new InputStreamReader(bis, StandardCharsets.UTF_8));
+
+            while (br.ready()) {
+                input.append(br.readLine());
+                input.append(" ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bis.close();
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        parseInput(input.toString());
+
+        long duration = System.nanoTime() - startTime;
+
+        System.out.println("The threatest product is " + 1 + ". Runtime took around " + duration
+                + " nanoseconds.");
+    }
+}
