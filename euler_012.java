b7be61ff66342b2562794c777d7adc470c8cca33
hchulvej <hchulvej@gmail.com>
2023-09-02 17:16:28 +0200

    Not done

--- euler_012.java
+++ euler_012.java
@@ -1,28 +1,28 @@
+import java.util.*;
+
 public class euler_012 {
     
-    private static long triNum(long n) {
-        return n * (n + 1) / 2;
-    }
-
     private static int noOfFactors(long n) {
-        int count = 0;
-        for (int d = 1; d <= n; d++) {
-            if (n % (long) d == 0) {
-                count++;
-            }
+        List<Integer> factors = new ArrayList<Integer>();
+        factors.add(1);
+        factors.add(n);
+
+        for (int i = 2; i < n; i++) {
+            
         }
-        return count;
+
+        return factors.size();
     }
 
     public static void main(String[] args) {
         
         long startTime = System.nanoTime();
 
-        System.out.println(noOfFactors(28));
+        
 
         long duration = System.nanoTime() - startTime;
 
-        System.out.println("The first triangle number with more than 100 factors is " + 1 + ". Runtime took around " + duration
+        System.out.println("The first triangle number with more than 100 factors is " + triNum(n) + ". Runtime took around " + duration
                 + " nanoseconds.");
     }
 }
