66a2f6d104f1988453da22cf48a0ef65db169395
hchulvej <hchulvej@gmail.com>
2024-09-20 14:53:06 +0200

    Finished 113

--- euler_113.py
+++ euler_113.py
@@ -1,56 +1,14 @@
-def is_increasing(n):
-    if n < 10:
-        return True
-    digits = [int(d) for d in str(n)]
-    for i in range(len(digits) - 1):
-        if digits[i] > digits[i + 1]:
-            return False
-    return True
+from math import comb
+## Number of increasing numbers with N digits is C(N+8,8)
+## Number of descreasing numbers with N digits is C(N+9,9) - 1
+## Numbers with only one repeating digit are counted twice, so we subtract 9 each time
+def non_bouncy(N: int) -> int:
+    return comb(N + 8, 8) + comb(N + 9, 9) - 10
 
-def is_decreasing(n):
-    if n < 10:
-        return True
-    digits = [int(d) for d in str(n)]
-    for i in range(len(digits) - 1):
-        if digits[i] < digits[i + 1]:
-            return False
-    return True
-
-limit = 100
-
-increasing_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-inc_digits = 1
-no_non_bouncy_numbers = 9
-
-def add_digit_inc(increasing_numbers):
-    res = []
-    for n in increasing_numbers:
-        for i in range(10):
-            if i >= n % 10 and is_increasing(n * 10 + i):
-                res.append(n * 10 + i)
+def non_bouncy_below(N: int) -> int:
+    res = 0
+    for n in range(1, N + 1):
+        res += non_bouncy(n)
     return res
 
-while inc_digits < limit:
-    increasing_numbers = add_digit_inc(increasing_numbers)
-    inc_digits += 1
-    no_non_bouncy_numbers += len(increasing_numbers)
-    
-decreasing_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-dec_digits = 1
-
-def add_digit_dec(decreasing_numbers):
-    res = []
-    for n in decreasing_numbers:
-        for i in range(10):
-            if i <= n % 10 and is_decreasing(n * 10 + i):
-                res.append(n * 10 + i)
-    return res
-
-while dec_digits < limit:
-    decreasing_numbers = add_digit_dec(decreasing_numbers)
-    dec_digits += 1
-    no_non_bouncy_numbers += len(decreasing_numbers)
-
-no_non_bouncy_numbers -= 9*(limit - 1)
-
-print(no_non_bouncy_numbers)
+print(non_bouncy_below(100))
