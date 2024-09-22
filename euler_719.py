d174ab3108ecee1c88f0aed1c9e63eb2e627459b
hchulvej <hchulvej@gmail.com>
2024-09-22 11:21:22 +0200

    Not done 719

--- euler_719.py
+++ euler_719.py
@@ -1,12 +1,25 @@
 from time import time
+from math import sqrt
+import functools
 
-def split_sums(N: int) -> list[int]:
-    nd = 
+memo = {}
+
+def partitions(s):
+    if len(s) > 0:
+        for i in range(1, len(s)+1):
+            first, rest = s[:i], s[i:]
+            for p in partitions(rest):
+                yield [first] + p
+    else:
+        yield []
+
+@functools.lru_cache
+def is_s_number(N: int) -> bool:
+    return int(sqrt(N)) in [sum(list(map(int, p))) for p in partitions(str(N)) if len(p) > 1]
 
 start = time()
 
-
-
+print(sum([n**2 for n in range(1, 1000001) if is_s_number(n**2)]))
 
 end = time()
 
