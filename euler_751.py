32bb35a84d23ab8cb4164168f3a532a23ccbdf90
hchulvej <hchulvej@gmail.com>
2024-09-24 11:51:29 +0200

    Not done 751

--- euler_751.py
+++ euler_751.py
@@ -22,8 +22,16 @@ def difference(phi):
 
 start = time()
 
-phi_ex = 2.21
-print([(float("2."+str(t)), difference(float("2."+str(t)))) for t in range(1, 10)])
+def add_digit(l: list[float]) -> list[float]:
+    d = len(str(l[0]))
+    candidates = [(f +float(t/(10**(d-2))), difference(f +float(t/(10**(d-2))))) for t in range(1, 10) for f in l]
+    candidates.sort(key=lambda x: abs(x[1]))
+    return [c[0] for c in candidates[:2]]
+
+phi_ex = 2.0
+candidates = add_digit([phi_ex])
+candidates = add_digit(candidates)
+print(candidates)
 
 end = time()
 print("Time: " + str(end - start) + " seconds")
