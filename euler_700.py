7b34f571984dc893a783a1a75d70d4741c51cffe
hchulvej <hchulvej@gmail.com>
2024-09-24 09:23:31 +0200

    Updated 700

--- euler_700.py
+++ euler_700.py
@@ -21,8 +21,20 @@ while len(eulercoins) < 16:
 
 print(eulercoins[-1])
 # Eulercoin no. 16 is 15806432 corresponding to n = 42298633
+# If Mn mod N is another Eulercoin, then Mn mod N < 15806432
+# and n > 42298633.
+# If Mn = k mod N, then n = (M^-1)k mod N.
+# We check k < 15806432 and note each corresponding n
 
-## NOT DONE - TOO INEFFICIENT
+invM = invmod(M, N)
+candidates = {(invM * k) % N : k for k in range(1, 15806432)}
+
+# løb over n'erne i voksende rækkefølge (næste ...)
+for k in range(15806431,1,-1):
+    if k < eulercoins[-1][0] and candidates[k] > eulercoins[-1][1]:
+        eulercoins.append((k, candidates[k]))
+        print(str(eulercoins[-2]) + " -> " + str(eulercoins[-1]))
+
 
 end = time()
 print("Time: " + str(end - start) + " seconds")
