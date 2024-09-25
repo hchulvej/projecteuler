eb7725b1ee6a3b19c72bf04b66ae8a566cacbbad
hchulvej <hchulvej@gmail.com>
2024-09-25 19:16:42 +0200

    Updated 700

--- euler_751.py
+++ euler_751.py
@@ -3,14 +3,14 @@ from decimal import Decimal, getcontext, ROUND_FLOOR
 from typing import Generator
 
 getcontext().prec = 102
-getcontext().rounding = ROUND_FLOOR
+#getcontext().rounding = ROUND_FLOOR
 
 def generate(phi: Decimal) -> Generator[Decimal, None, None]:
     b = phi
     a = round(b,0)
     while True:
         yield a
-        b = round(b,0)*(b - round(b,0) + 1)
+        b = round(b,0)*(b - round(b,0) + Decimal(1))
         a = round(b,0)
 
 def concatenate(phi: Decimal) -> Decimal:
@@ -23,12 +23,16 @@ def concatenate(phi: Decimal) -> Decimal:
     return Decimal(s)
 
 def add_digit(candidates: list[Decimal], digits: int) -> list[Decimal]:
-    new_candidates = [round(Decimal(str(c) + str(d)), digits) for c in candidates for d in range(10)]
-    return new_candidates
+    new_candidates = [Decimal(str(c)[:digits] + str(d)) for c in candidates for d in range(10)]
+    new_candidates.sort(key=lambda x: abs(concatenate(x)-x))
+    return new_candidates[:2]
 
 start = time()
 
-print(add_digit([Decimal(2.1)], 3))
+candidates = [Decimal("2." + str(d)) for d in range(10)]
+for d in range(23):
+    candidates = add_digit(candidates, d + 3)
+print(candidates[0])
 
 
 end = time()
