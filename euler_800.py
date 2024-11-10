43a4dfe59f1818d6b97116fe4117c3e4687f1d70
hchulvej <hchulvej@gmail.com>
2024-11-10 13:11:49 +0100

    800 Hybrid Integers WIP

--- euler_800.py
+++ euler_800.py
@@ -1,18 +1,29 @@
 from time import time
-from scipy.special import lambertw
 from math import log, exp, comb
-from sympy import primerange
+from gmpy2 import next_prime
+
+def prime_gen():
+    n = 5
+    while True:
+        yield n
+        n = next_prime(n)
 
 start = time()
 
+primes = [2, 3]
+pg = prime_gen()
+
 limit = 800
 n = limit * log(limit)
-prime_limit = 1000
-primes = list(primerange(2, prime_limit + 1))
-p = primes[-2]
-q = primes[-1]
-h = (p ** q) * (q ** p)
-print(log(h) >= n)
+
+prime_limit = primes[-1]*log(primes[-1]) + primes[-2]*log(primes[-2])
+
+while prime_limit < n:
+    primes.append(next(pg))
+    prime_limit = primes[-1]*log(primes[-1]) + primes[-2]*log(primes[-2])
+
+
+print(n, prime_limit)
 
 end = time()
 print("Time: " + str(end - start) + " seconds")
