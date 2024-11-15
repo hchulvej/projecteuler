8d13d8db8aa0a241d6993360097b3341581a6a42
hchulvej <hchulvej@gmail.com>
2024-11-15 14:06:02 +0100

    387 Harshad Numbers WIP

--- euler_387.py
+++ euler_387.py
@@ -3,28 +3,28 @@ from gmpy2 import is_prime
 
 start = time()
 
-# Right-truncable Harshad numbers
+def is_Harshad(v, s):
+    return v % s == 0
+
+# Right Truncattable Harshad numbers
 RTH_numbers = dict()
-for nd in range(1,13):
+for nd in range(1,14):
     RTH_numbers[nd] = []
 
 for d in range(1,10):
-    RTH_numbers[1].append((d,d))
+    RTH_numbers[1].append((d,d))   
 
-for n in range(2,13):
-    for d in range(10):    
-        for h in RTH_numbers[n - 1]:
-            if (10*h[0] + d) % (h[1] + d) == 0:
-                RTH_numbers[n].append((10*h[0] + d, h[1] + d))
+def add_digit_to_Harshad(nd):
+    existing_numbers = RTH_numbers[nd - 1]
+    for e in existing_numbers:
+        for d in range(0,10):
+            if is_Harshad(e[0] * 10 + d, e[1] + d):
+                RTH_numbers[nd].append((e[0] * 10 + d, e[1] + d))
 
-# Strong right-truncable Harshad numbers
-RTSH_numbers = dict()
-for nd in range(2,13):
-    RTSH_numbers[nd] = [h for h in RTH_numbers[nd] if is_prime(h[0] // h[1])]
+for nd in range(2,14):
+    add_digit_to_Harshad(nd)
 
-# Strong right-truncable Harshad primes
-
-
+print(len(RTH_numbers[13]))
 
 end = time()
 print("Time: " + str(end - start) + " seconds")
