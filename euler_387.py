1c5ce3022296fccf7980b73e63b0f0001fbae64d
hchulvej <hchulvej@gmail.com>
2024-11-16 12:56:35 +0100

    387 Harshad Primes Done

--- euler_387.py
+++ euler_387.py
@@ -24,7 +24,22 @@ def add_digit_to_Harshad(nd):
 for nd in range(2,14):
     add_digit_to_Harshad(nd)
 
-print(len(RTH_numbers[13]))
+# Strong, right truncatable Harshad numbers
+SRTH_numbers = []
+for nd in range(2,14):
+    for e in RTH_numbers[nd]:
+        if is_prime(e[0] // e[1]):
+            SRTH_numbers.append(e)
+
+# Strong, right truncatable Harshad primes
+SRTH_primes = []
+for e in SRTH_numbers:
+    for d in range(10):
+        if is_prime(e[0] * 10 + d):
+            SRTH_primes.append(e[0] * 10 + d)
+
+res = sum([p for p in SRTH_primes if p < 10**14])
 
 end = time()
+print("Sum: " + str(res))
 print("Time: " + str(end - start) + " seconds")
