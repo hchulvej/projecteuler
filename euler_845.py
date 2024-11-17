1696e6188b18372c510f61d2a83f72fbe61ae8c0
hchulvej <hchulvej@gmail.com>
2024-11-17 16:04:56 +0100

    845 Prime Digit Sum WIP

--- euler_845.py
+++ euler_845.py
@@ -1,5 +1,14 @@
 from time import time
-from gmpy2 import is_prime
+from gmpy2 import next_prime
+from itertools import permutations
+import numpy as np
+
+def prime_generator():
+    p = 2
+    while True:
+        yield p
+        p = next_prime(p)
+
 
 partitions = dict()
 partitions[1] = [[1]]
@@ -10,13 +19,49 @@ def get_partitions(n):
     except KeyError:
         pass
     partitions[n] = []
-    for p in range(1, n + 1):
-        for part in get_partitions(n - p):
-            partitions[n].append(part + [p])
+    x = [0] * (n + 1)
+    for i in range(1, n + 1):
+        x[i] = 1
+    partitions[n].append(x[1:])
+    x[0] = -1
+    x[1] = 2
+    h = 1
+    m = n - 1
+    partitions[n].append(x[1:(m + 1)])
+    while x[1] != n:
+        if m - h > 1:
+            h += 1
+            x[h] = 2
+            m -= 1
+        else:
+            j = m - 2
+            while x[j] == x[m - 1]:
+                x[j] = 1
+                j -= 1
+            h = j + 1
+            x[h] = x[m - 1] + 1
+            r = x[m] + x[m - 1] * (m - h - 1)
+            x[m] = 1
+            m = h + r - 1
+        partitions[n].append(x[1:(m + 1)])
+    return partitions[n]
+
+def partition_to_number(partition):
+    digits = list(map(str, partition))
+    numbers = np.empty(len(digits), dtype=np.int16)
+    for c in permutations(digits, len(digits)):
+        num = int("".join(c))
+        print(num)
+        np.append(numbers, num)
+    return numbers
 
 start = time()
 
-print(get_partitions(13))
+n = 61
+pg = prime_generator()
+numbers = set()
+
+print([list(partition_to_number(partition)) for partition in get_partitions(5)])
 
 end = time()
 print("Time: " + str(end - start) + " seconds")
